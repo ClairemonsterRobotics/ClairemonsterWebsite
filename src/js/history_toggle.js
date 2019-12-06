ae45384cd9135e7813b646a35d2407b1eb3cae7b
Keagan Holmes <keaganchs@gmail.com>
2019-12-05 16:08:50 -0800

    Currently what's on the website.
    Had to do some troubleshooting, changes are fixed!

--- src/js/history_toggle.js
+++ src/js/history_toggle.js
@@ -4,8 +4,8 @@ function history_button(year) {
     var elmnt = document.getElementById("article-title");
     scroll(elmnt); 
 }
-
 function close_others(){
+/* These are the lines you want to copy/paste. Make sure the element IDs are the same. */
   document.getElementById("article_2015").style.display = "none";
   document.getElementById("article_2016").style.display = "none";
   document.getElementById("article_2017").style.display = "none";
@@ -15,7 +15,7 @@ function close_others(){
 
 window.onload = function() {
     close_others();
-    document.getElementById("article_2018").style.display = "";
+    document.getElementById("article_2019").style.display = "";
 }
 
 function expandCard(year){
